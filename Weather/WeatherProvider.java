c0a68352e4aecfa2fba30a8e6c3dcbf4f3ddbf64
unknown <timothyclintonwebb@gmail.com>
2020-09-03 17:06:48 +0200

    Added weatherprovider, need to double check if it works

---
+++ Weather/WeatherProvider.java
@@ -0,0 +1,24 @@
+package Weather;
+
+import java.util.Random;
+
+import Aircraft.Coordinates;
+
+public class WeatherProvider {
+    
+    private static WeatherProvider weatherProvider = new WeatherProvider();
+    private static String[] weather = {"SUN", "RAIN", "SNOW", "FOG"};
+
+    private WeatherProvider() {
+    }
+
+    public static WeatherProvider getProvider() {
+        return weatherProvider;
+    }
+
+    public String getCurrentWeather(Coordinates coordinates) {
+        int rand = new Random().nextInt(3);//double check if 3 or 4
+        return weather[rand];
+    }
+
+}
