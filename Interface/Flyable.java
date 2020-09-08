f6c2e9c4bebb08cf9049ccd759fbbbff0ddf2a5a
unknown <timothyclintonwebb@gmail.com>
2020-09-08 11:06:03 +0200

    Added Flyable.java

---
+++ Interface/Flyable.java
@@ -0,0 +1,13 @@
+package Interface;
+
+import Weather.WeatherTower;
+
+public class Flyable {
+    
+    public void updateConditions() {
+    }
+
+    public void registerTower(WeatherTower weatherTower) {
+    }
+
+}
